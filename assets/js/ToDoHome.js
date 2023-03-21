80cbccf8dbf7468a3efeed157338f6540bfc4bd0
Animesh Raj <animesh080808@gmail.com>
2023-03-22 00:28:16 +0530

    ✨ Feat (/assets/js/ToDoHome.js) : No Need of particle js file for this todo everything handled in html and css

--- assets/js/ToDoHome.js
+++ assets/js/ToDoHome.js
@@ -1,13 +1,1 @@
-console.clear();
-
-try {
-    if (document.querySelector('input[type="checkbox"]') != null) {
-        setTimeout(function () {
-            document
-                .querySelector('input[type="checkbox"]')
-                .setAttribute('checked', true);
-        }, 100);
-    }
-} catch (error) {
-    console.error(error);
-}
+// We Dont Need this Js for the Todo
